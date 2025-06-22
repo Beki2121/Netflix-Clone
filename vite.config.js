9bf4e5979730bcb6455077e73c938adc54b40758
Beki2121 <berekethabtamu2121@gmail.com>
2025-06-22 12:21:51 +0300

    Prepare for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/Netflix-Clone-2025/", // 👈 must match repo name
+  base: "/Netflix-Clone/", // 👈 must match repo name
 });
